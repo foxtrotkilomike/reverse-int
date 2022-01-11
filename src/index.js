b8a3e9f1f599c20c3cc4b0235604552e1cdab3bf
Philipp Khromov <filooloog@mail.ru>
2022-01-11 14:41:17 +0300

    feat: implement 'reverse' function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let numStr = n.toString();
+  numStr = numStr.includes('-') ? numStr.replace('-','') : numStr;
+  numStr = [...numStr];
+  let numReverse = '';
+
+  for (let i = numStr.length; i > 0; i--) {
+    numReverse += numStr[i - 1];
+  }
+
+  return +numReverse;
 }
